d1e7c43e315b20368b35396bb68ee608a3901958
Luis D. Maco <luisdmaco@outlook.com>
2023-06-07 12:46:28 -0500

    Save resources generated

---
+++ PCInfo_backend/Recommendations/Resource/SaveMonitorResource.cs
@@ -0,0 +1,10 @@
+﻿namespace PCInfo_backend.Recommendations.Resource;
+
+public class SaveMonitorResource
+{
+    public string resolucion { get; set; }
+    public int pulgadas { get; set; }
+    public string HDR { get; set; }
+    public string conexiones { get; set; }
+    public int codigoProducto { get; set; }
+}
---
+++ PCInfo_backend/Recommendations/Resource/SaveProductoResource.cs
@@ -0,0 +1,10 @@
+﻿namespace PCInfo_backend.Recommendations.Resource;
+
+public class SaveProductoResource
+{
+    public string Nombre { get; set; }
+    public string Marca { get; set; }
+    public double Precio { get; set; }
+    public string Descripcion { get; set; }
+    public int Stock { get; set; }
+}
