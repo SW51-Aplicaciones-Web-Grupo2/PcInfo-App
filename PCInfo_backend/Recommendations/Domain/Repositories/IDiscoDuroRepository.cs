8400ca90b72ba01cb2c944b7de9c4ba74ed3c1d6
Luis D. Maco <luisdmaco@outlook.com>
2023-06-07 14:54:35 -0500

    Repositories generated

---
+++ PCInfo_backend/Recommendations/Domain/Repositories/IDiscoDuroRepository.cs
@@ -0,0 +1,13 @@
+﻿using PCInfo_backend.Recommendations.Domain.Models;
+
+namespace PCInfo_backend.Recommendations.Domain.Repositories;
+
+public interface IDiscoDuroRepository
+{
+    Task<IEnumerable<DiscoDuro>> ListAsync();
+    Task AddAsync(DiscoDuro discoDuro);
+    Task<DiscoDuro> FindByIdAsync(int id);
+    void Update(DiscoDuro discoDuro);
+    void Remove(DiscoDuro discoDuro);
+    
+}
