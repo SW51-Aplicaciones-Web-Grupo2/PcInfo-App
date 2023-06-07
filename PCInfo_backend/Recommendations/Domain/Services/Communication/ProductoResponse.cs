ae896f05145a3618bd1681fbc412b123bc446535
Luis D. Maco <luisdmaco@outlook.com>
2023-06-07 12:05:18 -0500

    Response classes generated

---
+++ PCInfo_backend/Recommendations/Domain/Services/Communication/ProductoResponse.cs
@@ -0,0 +1,15 @@
+﻿using PCInfo_backend.Recommendations.Domain.Models;
+using PCInfo_backend.Shared.Domain.Services.Communication;
+
+namespace PCInfo_backend.Recommendations.Domain.Services.Communication;
+
+public class ProductoResponse : BaseResponse<Producto>
+{
+    public ProductoResponse(string message) : base(message)
+    {
+    }
+
+    public ProductoResponse(Producto resource) : base(resource)
+    {
+    }
+}
