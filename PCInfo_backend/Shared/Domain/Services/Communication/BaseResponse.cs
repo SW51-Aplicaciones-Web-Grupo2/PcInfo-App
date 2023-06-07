cf8c36ac9e48773b3da3bc5ea3039d4a095c7d8f
Luis D. Maco <luisdmaco@outlook.com>
2023-06-07 11:59:01 -0500

    BaseResponse class generated

---
+++ PCInfo_backend/Shared/Domain/Services/Communication/BaseResponse.cs
@@ -0,0 +1,19 @@
+﻿namespace PCInfo_backend.Shared.Domain.Services.Communication;
+
+public abstract class BaseResponse<T>
+{
+    protected BaseResponse(string message)
+    {
+        Message = message;
+    }
+    
+    protected BaseResponse(T resource)
+    {
+        Resource = resource;
+    }
+    
+    public bool Success { get; set; }
+    public string Message { get; set; }
+    public T Resource { get; set; }
+    
+}
