cedde76f19f5cd5f21bc442918b3d103ff7d8889
Luis D. Maco <luisdmaco@outlook.com>
2023-06-10 00:35:36 -0500

    Resources and SaveResources generated

---
+++ Backend/PCInfo_backend/Recommendations/Resource/ClienteResource.cs
@@ -0,0 +1,14 @@
+﻿using PCInfo_backend.Recommendations.Domain.Models.Users;
+
+namespace PCInfo_backend.Recommendations.Resource;
+
+public class ClienteResource
+{
+    public string DNI { get; set; }
+    public string Nombre { get; set; }
+    public string Apellidos { get; set; }
+    public string Telefono { get; set; }
+    public string Correo { get; set; }
+    public string Direccion { get; set; }
+    public User user { get; set; }
+}
