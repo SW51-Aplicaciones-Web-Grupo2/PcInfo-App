f32e8c55d2054d925567662ec6f56289c9a8dc77
Luis D. Maco <luisdmaco@outlook.com>
2023-06-10 00:07:27 -0500

    Cliente and User generated, repository interfaces generated

---
+++ Backend/PCInfo_backend/Recommendations/Domain/Repositories/Users/IClienteRepository.cs
@@ -0,0 +1,13 @@
+﻿using PCInfo_backend.Recommendations.Domain.Models.Users;
+
+namespace PCInfo_backend.Recommendations.Domain.Repositories.Users;
+
+public interface IClienteRepository
+{
+    Task<IEnumerable<Cliente>> ListAsync();
+    Task AddAsync(Cliente cliente);
+    Task<Cliente> FindByIdAsync(int id);
+    void Update(Cliente cliente);
+    void Remove(Cliente cliente);
+    
+}
