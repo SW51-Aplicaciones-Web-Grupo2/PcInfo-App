3c29cfdbbb5a54cee17e092e6c4e81ed4d0b587b
Luis D. Maco <luisdmaco@outlook.com>
2023-06-10 00:23:35 -0500

    ClienteResponse and UserResponse created

---
+++ Backend/PCInfo_backend/Recommendations/Domain/Services/Communication/ClienteResponse.cs
@@ -0,0 +1,14 @@
+﻿using PCInfo_backend.Recommendations.Domain.Models.Users;
+using PCInfo_backend.Shared.Domain.Services.Communication;
+
+namespace PCInfo_backend.Recommendations.Domain.Services.Communication;
+
+public class ClienteResponse : BaseResponse<Cliente>
+{
+    public ClienteResponse(string message) : base(message)
+    {
+    }
+    public ClienteResponse(Cliente resource) : base(resource)
+    {
+    }
+}
