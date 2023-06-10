fe24390920f35aab517f6ed85daedef593a05d08
Luis D. Maco <luisdmaco@outlook.com>
2023-06-10 00:14:14 -0500

    IUserService and IClienteService generated

---
+++ Backend/PCInfo_backend/Recommendations/Domain/Services/IClienteService.cs
@@ -0,0 +1,12 @@
+﻿using PCInfo_backend.Recommendations.Domain.Models.Users;
+
+namespace PCInfo_backend.Recommendations.Domain.Services;
+
+public interface IClienteService
+{
+    Task<IEnumerable<Cliente>> ListAsync();
+    Task<Cliente> GetAsync(int id);
+    Task<Cliente> SaveAsync(Cliente cliente);
+    Task<Cliente> UpdateAsync(int id, Cliente cliente);
+    Task<Cliente> DeleteAsync(int id);
+}
---
+++ Backend/PCInfo_backend/Recommendations/Domain/Services/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using PCInfo_backend.Recommendations.Domain.Models.Users;
+
+namespace PCInfo_backend.Recommendations.Domain.Services;
+
+public interface IUserService
+{
+    Task<IEnumerable<User>> ListAsync();
+    Task<User> GetAsync(string id);
+    Task<User> SaveAsync(User user);
+    Task<User> UpdateAsync(string id, User user);
+    Task<User> DeleteAsync(string id);
+}
