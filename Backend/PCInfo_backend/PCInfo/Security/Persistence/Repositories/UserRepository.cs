08b1f29562e1898bfd038c93e7524eed702fa7a6
Luis D. Maco <luisdmaco@outlook.com>
2023-06-28 17:31:04 -0500

    UserRepository class created

---
+++ Backend/PCInfo_backend/PCInfo/Security/Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,53 @@
+using Microsoft.EntityFrameworkCore;
+using PCInfo_backend.PCInfo.Security.Domain.Models;
+using PCInfo_backend.PCInfo.Security.Domain.Repositories;
+using PCInfo_backend.Shared.Persistence;
+
+namespace PCInfo_backend.PCInfo.Security.Persistence.Repositories;
+
+public class UserRepository : BaseRepository, IUserRepository
+{
+    public UserRepository(AppDbContext context) : base(context)
+    {
+    }
+
+    public async Task<IEnumerable<User>> ListAsync()
+    {
+        return await _context.Users.ToListAsync();
+    }
+
+    public async Task AddAsync(User user)
+    {
+        await _context.Users.AddAsync(user);
+    }
+
+    public async Task<User> FindByIdAsync(int id)
+    {
+        return await _context.Users.FindAsync(id);
+    }
+
+    public async Task<User> FindByUsernameAsync(string username)
+    {
+        return await _context.Users.SingleOrDefaultAsync(x => x.Username == username);
+    }
+
+    public bool ExistsByUsername(string username)
+    {
+        return _context.Users.Any(x => x.Username == username);
+    }
+
+    public User FindById(int id)
+    {
+        return _context.Users.Find(id);
+    }
+
+    public void Update(User user)
+    {
+        _context.Users.Update(user);
+    }
+
+    public void Remove(User user)
+    {
+        _context.Users.Remove(user);
+    }
+}
