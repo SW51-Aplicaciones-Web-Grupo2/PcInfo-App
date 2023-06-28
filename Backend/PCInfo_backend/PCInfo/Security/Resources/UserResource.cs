1b172b3fdb16338d59090e8528a7dc26123ddb29
Luis D. Maco <luisdmaco@outlook.com>
2023-06-28 16:46:01 -0500

    UserResource created

---
+++ Backend/PCInfo_backend/PCInfo/Security/Resources/UserResource.cs
@@ -0,0 +1,9 @@
+namespace PCInfo_backend.PCInfo.Security.Resources;
+
+public class UserResource
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Username { get; set; } 
+}
