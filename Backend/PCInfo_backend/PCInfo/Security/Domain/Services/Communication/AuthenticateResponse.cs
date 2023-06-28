02fd066fcafd26240dbd00c3c04d6f46e50f9083
Luis D. Maco <luisdmaco@outlook.com>
2023-06-28 16:51:03 -0500

    RegisterRequest class created

--- Backend/PCInfo_backend/PCInfo/Security/Domain/Services/Communication/AuthenticateResponse.cs
+++ Backend/PCInfo_backend/PCInfo/Security/Domain/Services/Communication/AuthenticateResponse.cs
@@ -3,6 +3,8 @@ namespace PCInfo_backend.PCInfo.Security.Domain.Services.Communication;
 public class AuthenticateResponse
 {
     public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
     public string Username { get; set; }
     public string Token { get; set; }
     
